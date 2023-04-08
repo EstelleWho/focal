9cb0ee356d804f45c8656f13956606172e5f5001
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-04-08 12:34:32 -0400

    first commit

---
+++ dice-roller.js
@@ -0,0 +1,4 @@
+//Write a program that takes a single parameter from the command line, a number, and rolls that many six-sided dice.
+
+let random = Math.floor(Math.random() * 6) + 1;
+console.log(random);
