0ccaadbc1f229bbd2976ca30ad3f8d8fe8104f0d
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-04-29 00:36:12 -0400

    added code

--- dice-roller.js
+++ dice-roller.js
@@ -1,4 +1,14 @@
 //Write a program that takes a single parameter from the command line, a number, and rolls that many six-sided dice.
 
-let random = Math.floor(Math.random() * 6) + 1;
-console.log(random);
+//let random = Math.floor(Math.random() * 6) + 1;
+//console.log(random);
+
+
+const numDice = Number(process.argv[2]);
+
+const rolls = [];
+for (let i = 0; i < numDice; i++) {
+  rolls.push(Math.floor(Math.random() * 6) + 1);
+}
+
+console.log(`Rolled ${numDice} dice: ${rolls.join(', ')}`);
